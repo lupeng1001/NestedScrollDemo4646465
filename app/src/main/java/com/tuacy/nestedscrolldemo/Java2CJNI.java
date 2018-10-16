69b02247c9f5fcc1f28d224ca8a8727a0083bd0e
lupeng1001 <741056371@qq.com>
2018-10-16 15:38:20 +0800

    测试新clone下来的项目更新远程库代码

--- app/src/main/java/com/tuacy/nestedscrolldemo/Java2CJNI.java
+++ app/src/main/java/com/tuacy/nestedscrolldemo/Java2CJNI.java
@@ -1,11 +1,15 @@
 package com.tuacy.nestedscrolldemo;
 
+import android.util.Log;
+
 /**
  * Created by Administrator on 2018/10/15.
  */
 
 public class Java2CJNI {
+    private static final String TAG = "Java2CJNI";
     static {
+        Log.e(TAG,"static部分代码开始执行");
         System.loadLibrary("java-lib");
     }
     public native String Java2C();
